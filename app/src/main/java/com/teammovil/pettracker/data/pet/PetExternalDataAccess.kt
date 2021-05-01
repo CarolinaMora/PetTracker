02e12d155a492d2bb52d3e13515577bb5f3a7486
Carolina Caballero <ccaballero@neitek.com>
2021-05-01 14:28:15 -0500

    Pantalla de Envio de Evidencia, Falto PetExternalDataAccess.kt y PetFakesDataAccess.kt

--- app/src/main/java/com/teammovil/pettracker/data/pet/PetExternalDataAccess.kt
+++ app/src/main/java/com/teammovil/pettracker/data/pet/PetExternalDataAccess.kt
@@ -1,5 +1,6 @@
 package com.teammovil.pettracker.data.pet
 
+import com.teammovil.pettracker.domain.Evidence
 import com.teammovil.pettracker.domain.Pet
 
 interface PetExternalDataAccess {
@@ -9,4 +10,6 @@ interface PetExternalDataAccess {
     suspend fun getPetById (petId: String): Pet
 
     suspend fun registerPet (pet: Pet): Boolean
+
+    suspend fun saveEvidence (petId: String, evidence: Evidence): Boolean
 }
