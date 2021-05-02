90521627701aee080fe3bfe0dfd7e29fdf6f2e31
CarolinaMoraRo <cmora@a4sys.com>
2021-05-01 22:39:19 -0500

    Merge: Conflictos por data binding y clase FieldView

--- app/src/main/java/com/teammovil/pettracker/ui/sendevidence/SendEvidenceViewModelFactory.kt
+++ app/src/main/java/com/teammovil/pettracker/ui/sendevidence/SendEvidenceViewModelFactory.kt
@@ -3,7 +3,6 @@ package com.teammovil.pettracker.ui.sendevidence
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.teammovil.pettracker.data.pet.PetRepository
-import com.teammovil.pettracker.ui.petregistration.PetRegistrationViewModel
 
 class SendEvidenceViewModelFactory(var petRepository: PetRepository) : ViewModelProvider.Factory{
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
