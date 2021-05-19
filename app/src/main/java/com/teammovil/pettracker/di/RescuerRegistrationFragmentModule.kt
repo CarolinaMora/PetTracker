4c7b72f4282fb47ff6f9a87eb22ed8f3fe39a253
CarolinaMoraRo <cmora@a4sys.com>
2021-05-18 21:53:05 -0500

    Issue 69: It quit provideRescuerRegistrationViewModel function in RescuerRegistrationFragmentModule.kt because it's already defined by @Inject constructor directly in RescuerRegistrationViewModel

--- app/src/main/java/com/teammovil/pettracker/di/RescuerRegistrationFragmentModule.kt
+++ app/src/main/java/com/teammovil/pettracker/di/RescuerRegistrationFragmentModule.kt
@@ -1,7 +1,6 @@
 package com.teammovil.pettracker.di
 
 import com.teammovil.data.rescuer.RescuerRepository
-import com.teammovil.pettracker.ui.rescuerregistration.RescuerRegistrationViewModel
 import com.teammovil.usecases.registerrescuer.RegisterRescuerUseCase
 import dagger.Module
 import dagger.Provides
@@ -11,10 +10,7 @@ import dagger.hilt.android.components.ActivityRetainedComponent
 @Module
 @InstallIn(ActivityRetainedComponent::class)
 class RescuerRegistrationFragmentModule {
-
-    @Provides
-    fun provideRescuerRegistrationViewModel (registerRescuerUseCase: RegisterRescuerUseCase) = RescuerRegistrationViewModel(registerRescuerUseCase)
-
+    
     @Provides
     fun provideRegisterRescuerUseCase (rescuerRepository: RescuerRepository) = RegisterRescuerUseCase(rescuerRepository)
 
