bc671b392f0a6a53c3a57af86693d50d2664b56e
j_crespo <jcrespo@likenuuk.com>
2021-05-20 21:55:47 -0500

    se agrego la inyeccion de dependencias a RegisteredPetsFragment y se elimino el ViewModelFactory

---
+++ app/src/main/java/com/teammovil/pettracker/di/registeredpets/RegisteredPetsModule.kt
@@ -0,0 +1,23 @@
+package com.teammovil.pettracker.di.registeredpets
+
+import com.teammovil.data.pet.PetRepository
+import com.teammovil.data.rescuer.RescuerRepository
+import com.teammovil.pettracker.ui.registeredpets.RegisteredPetsViewModel
+import com.teammovil.usecases.rescuerPets.GetRescuerPets
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class RegisteredPetsModule {
+
+    @Provides
+    fun rescuerPetsViewModelProvider(getRescuerPets: GetRescuerPets) = RegisteredPetsViewModel(getRescuerPets)
+
+    @Provides
+    @ViewModelScoped
+    fun getRescuerPetProvider(petRepository: PetRepository, rescuerRepository: RescuerRepository) = GetRescuerPets(petRepository, rescuerRepository)
+}
--- app/src/main/java/com/teammovil/pettracker/ui/registeredpets/RegisteredPetsViewModel.kt
+++ app/src/main/java/com/teammovil/pettracker/ui/registeredpets/RegisteredPetsViewModel.kt
@@ -6,11 +6,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.teammovil.pettracker.ui.common.Event
 import com.teammovil.usecases.rescuerPets.GetRescuerPets
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class RegisteredPetsViewModel(private val getRescuerPets: GetRescuerPets) : ViewModel() {
+@HiltViewModel
+class RegisteredPetsViewModel @Inject constructor(private val getRescuerPets: GetRescuerPets) : ViewModel() {
 
     sealed class UiModel {
         object Loading : UiModel()
