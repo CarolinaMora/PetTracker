ecb53d27861b2d675605e45299e70cd89f27f280
CarolinaMoraRo <cmora@a4sys.com>
2021-06-02 21:56:38 -0500

    issue 117: Add tests to get 100% of coverage of edit pet use case. Use pet validator as a delegate

--- usecases/src/test/java/com/teammovil/usecases/EditPetUseCaseTest.kt
+++ usecases/src/test/java/com/teammovil/usecases/EditPetUseCaseTest.kt
@@ -3,11 +3,12 @@ package com.teammovil.usecases
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import com.teammovil.data.pet.PetRepository
+import com.teammovil.domain.Error
 import com.teammovil.domain.Result
-import com.teammovil.domain.rules.PetValidator
 import com.teammovil.testshared.mockPet
 import com.teammovil.usecases.editpet.EditPetUseCase
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -28,13 +29,50 @@ class EditPetUseCaseTest {
     }
 
     @Test
-    fun `EditPetUseCase updatePet repository called` (){
+    fun `EditPetUseCase property petRepository exists` (){
+        Assert.assertTrue(editPetUseCase.petRepository is PetRepository)
+    }
+
+    @Test
+    fun `EditPetUseCase valid when validation, updatePet return ok` (){
         runBlocking {
+            val spiedUseCase = com.nhaarman.mockitokotlin2.spy(editPetUseCase)
+            whenever(spiedUseCase.validatePet(mockPet)).thenReturn(Result(Unit, null))
             whenever(petRepository.updatePet(mockPet)).thenReturn(true)
 
-            editPetUseCase.invoke(mockPet)
+            val result = spiedUseCase.invoke(mockPet).success != null
 
+            verify(spiedUseCase).validatePet(mockPet)
             verify(petRepository).updatePet(mockPet)
+            Assert.assertTrue(result)
+        }
+    }
+
+    @Test
+    fun `EditPetUseCase invalid when validate pet returns failure` (){
+        runBlocking {
+            val spiedUseCase = com.nhaarman.mockitokotlin2.spy(editPetUseCase)
+            whenever(spiedUseCase.validatePet(mockPet)).thenReturn(Result(null, listOf(Error(0))))
+
+            val result = spiedUseCase.invoke(mockPet).success != null
+
+            verify(spiedUseCase).validatePet(mockPet)
+            Assert.assertFalse(result)
+        }
+    }
+
+    @Test
+    fun `EditPetUseCase invalid when updatePet returns failure` (){
+        runBlocking {
+            val spiedUseCase = com.nhaarman.mockitokotlin2.spy(editPetUseCase)
+            whenever(spiedUseCase.validatePet(mockPet)).thenReturn(Result(Unit, null))
+            whenever(petRepository.updatePet(mockPet)).thenReturn(false)
+
+            val result = spiedUseCase.invoke(mockPet).success != null
+
+            verify(spiedUseCase).validatePet(mockPet)
+            verify(petRepository).updatePet(mockPet)
+            Assert.assertFalse(result)
         }
     }
 
