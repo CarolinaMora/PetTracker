313b4568a2d6653e4740ba862ca82e3bccd0bf30
j_crespo <jcrespo@likenuuk.com>
2021-06-07 21:38:46 -0500

    Se cambio el llamado a la clase Validator de Rescuer y Evidence

--- domain/src/test/java/com/teammovil/domain/RescuerValidatorTest.kt
+++ domain/src/test/java/com/teammovil/domain/RescuerValidatorTest.kt
@@ -1,17 +1,26 @@
 package com.teammovil.domain
 
 import com.teammovil.domain.rules.RescuerValidator
+import com.teammovil.domain.rules.RescuerValidatorImpl
 import com.teammovil.domain.rules.RulesErrors
 import com.teammovil.testshared.mockRescuer
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 
 class RescuerValidatorTest {
 
+    lateinit var rescuerValidator: RescuerValidator
+
+    @Before
+    fun setUp(){
+        rescuerValidator = RescuerValidatorImpl()
+    }
+
     @Test
     fun `is valid rescuer in RescuerValidator`(){
         val rescuer = mockRescuer.copy()
-        val result = RescuerValidator.validateRescuer(rescuer).valid
+        val result = rescuerValidator.validateRescuer(rescuer).valid
 
         assertTrue(result)
     }
@@ -19,7 +28,7 @@ class RescuerValidatorTest {
     @Test
     fun `empty input name RescuerValidator`(){
         val rescuer = mockRescuer.copy(name = "")
-        val result = RescuerValidator.validateRescuer(rescuer).error?.get(0)?.code == RulesErrors.NAME_FIELD_EMPTY_ERROR
+        val result = rescuerValidator.validateRescuer(rescuer).error?.get(0)?.code == RulesErrors.NAME_FIELD_EMPTY_ERROR
 
         assertTrue(result)
     }
@@ -27,7 +36,7 @@ class RescuerValidatorTest {
     @Test
     fun `empty activityStartDate in RescuerValidator`(){
         val rescuer = mockRescuer.copy(activityStartDate = null)
-        val result = RescuerValidator.validateRescuer(rescuer).error?.get(0)?.code == RulesErrors.ACTIVITY_START_DATE_FIELD_EMPTY_ERROR
+        val result = rescuerValidator.validateRescuer(rescuer).error?.get(0)?.code == RulesErrors.ACTIVITY_START_DATE_FIELD_EMPTY_ERROR
 
         assertTrue(result)
     }
