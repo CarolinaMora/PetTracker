ef2c912298ea44006f3d04ce74ae1caaf81bb76a
CarolinaMoraRo <cmora@a4sys.com>
2021-06-11 22:21:57 -0500

    issue 136: tests of last four functions of pet repository

--- data/src/test/java/com/teammovil/data/PetRepositoryTest.kt
+++ data/src/test/java/com/teammovil/data/PetRepositoryTest.kt
@@ -1,8 +1,16 @@
 package com.teammovil.data
 
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
 import com.teammovil.data.pet.PetExternalDataAccess
 import com.teammovil.data.pet.PetRepository
+import com.teammovil.testshared.mockEvidence
+import com.teammovil.testshared.mockPet
+import com.teammovil.testshared.mockRescuer
+import junit.framework.Assert.assertTrue
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
@@ -18,6 +26,54 @@ class PetRepositoryTest {
     @Before
     fun setUp (){
         petRepository = PetRepository(petExternalDataAccess)
+    }
+
+    @Test
+    fun `register pet calls external data access` (){
+        runBlocking {
+            whenever(petExternalDataAccess.registerPet(mockPet, mockRescuer.email)).thenReturn(true)
+
+            val result = petRepository.registerPet(mockPet, mockRescuer.email)
+
+            verify(petExternalDataAccess).registerPet(mockPet, mockRescuer.email)
+            assertTrue(result)
+        }
+    }
+
+    @Test
+    fun `update pet calls external data access` (){
+        runBlocking {
+            whenever(petExternalDataAccess.updatePet(mockPet)).thenReturn(true)
+
+            val result = petRepository.updatePet(mockPet)
+
+            verify(petExternalDataAccess).updatePet(mockPet)
+            assertTrue(result)
+        }
+    }
+
+    @Test
+    fun `assign adopter to pet calls external data access` (){
+        runBlocking {
+            whenever(petExternalDataAccess.assignAdopterToPet(mockPet.id, mockRescuer.email)).thenReturn(true)
+
+            val result = petRepository.assignAdopterToPet(mockPet.id, mockRescuer.email)
+
+            verify(petExternalDataAccess).assignAdopterToPet(mockPet.id, mockRescuer.email)
+            assertTrue(result)
+        }
+    }
+
+    @Test
+    fun `save evidence calls external data access` (){
+        runBlocking {
+            whenever(petExternalDataAccess.saveEvidence(mockPet.id, mockEvidence)).thenReturn(true)
+
+            val result = petRepository.saveEvidence(mockPet.id, mockEvidence)
+
+            verify(petExternalDataAccess).saveEvidence(mockPet.id, mockEvidence)
+            assertTrue(result)
+        }
     }
     
 }
