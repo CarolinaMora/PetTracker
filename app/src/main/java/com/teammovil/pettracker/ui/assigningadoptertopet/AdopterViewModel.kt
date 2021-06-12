1d02868bc2c02cc8b1d9471f363f7344c7693867
Diego <estradadg7@gmail.com>
2021-06-11 20:19:00 -0500

    Issue / 140 assign adopter to pet, viewModel unit test was completed!

--- app/src/main/java/com/teammovil/pettracker/ui/assigningadoptertopet/AdopterViewModel.kt
+++ app/src/main/java/com/teammovil/pettracker/ui/assigningadoptertopet/AdopterViewModel.kt
@@ -2,27 +2,31 @@ package com.teammovil.pettracker.ui.assigningadoptertopet
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.teammovil.pettracker.ui.common.Event
+import com.teammovil.pettracker.ui.common.ScopedViewModel
 import com.teammovil.pettracker.util.MessageValidation
 import com.teammovil.usecases.assignadoptertopet.AssignAdopterToPetUseCase
 import com.teammovil.usecases.getalladopters.GetAllAdoptersUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class AdopterViewModel @Inject constructor(var assignAdopterToPetUseCase: AssignAdopterToPetUseCase, var getAllAdoptersUseCase: GetAllAdoptersUseCase):ViewModel() {
+class AdopterViewModel @Inject constructor(
+    var assignAdopterToPetUseCase: AssignAdopterToPetUseCase,
+    var getAllAdoptersUseCase: GetAllAdoptersUseCase,
+    uiDispatcher: CoroutineDispatcher
+) : ScopedViewModel(uiDispatcher) {
 
-    sealed class UiModel{
-        object Loading: UiModel()
-        class AdoptersContent(val adopter: List<com.teammovil.domain.Adopter>) :UiModel()
-        class SuccessNotification(val message: String) : UiModel()
-        class ErrorNotification(val message: String) : UiModel()
-        class SuccessConfimation(val adopterId: String, val adopterName: String): UiModel()
+    sealed class UiModel {
+        object Loading : UiModel()
+        data class AdoptersContent(val adopter: List<com.teammovil.domain.Adopter>) : UiModel()
+        data class SuccessNotification(val message: String) : UiModel()
+        data class ErrorNotification(val message: String) : UiModel()
+        data class SuccessConfimation(val adopterId: String, val adopterName: String) : UiModel()
     }
 
     private val _model = MutableLiveData<UiModel>()
@@ -31,51 +35,51 @@ class AdopterViewModel @Inject constructor(var assignAdopterToPetUseCase: Assign
     private val _navigation = MutableLiveData<Event<Unit>>()
     val navigation: LiveData<Event<Unit>> get() = _navigation
 
-    init {
-        viewModelScope.launch {
+    fun onStartView() {
+        launch {
             _model.value = UiModel.Loading
-            var result = withContext(Dispatchers.IO){getAllAdoptersUseCase.invoke()}
+            val result = withContext(Dispatchers.IO) { getAllAdoptersUseCase.invoke() }
             setView(result)
         }
     }
 
-    fun onClickOkAdvice (){
+    fun onClickOkAdvice() {
         _navigation.value = Event(Unit)
     }
 
-    fun onAdopterClicked(adopter: com.teammovil.domain.Adopter){
+    fun onAdopterClicked(adopter: com.teammovil.domain.Adopter) {
         showConfirmation(adopter.email, adopter.name)
     }
 
-    private fun showConfirmation (adopterId: String, adopterName: String){
-        _model.value = UiModel.SuccessConfimation(adopterId,adopterName)
+    private fun showConfirmation(adopterId: String, adopterName: String) {
+        _model.value = UiModel.SuccessConfimation(adopterId, adopterName)
     }
 
-    private fun showSuccessAdvice (){
+    private fun showSuccessAdvice() {
         _model.value = UiModel.SuccessNotification(MessageValidation.ADOPTER_REGISTER_SUCCESS)
     }
 
-    private fun showRegistrationError (){
+    private fun showRegistrationError() {
         _model.value = UiModel.ErrorNotification(MessageValidation.ADOPTER_REGISTER_FAILURE)
     }
 
-    private fun setView(adopterList: List<com.teammovil.domain.Adopter>){
+    private fun setView(adopterList: List<com.teammovil.domain.Adopter>) {
         _model.value = UiModel.AdoptersContent(adopterList)
     }
 
     fun onClickConfirmation(petId: String, adopterId: String) {
-        onAssignAdopterToPet(adopterId,petId)
+        onAssignAdopterToPet(adopterId, petId)
     }
 
-    fun onAssignAdopterToPet(adopterId: String, petId: String){
-        viewModelScope.launch {
+    fun onAssignAdopterToPet(adopterId: String, petId: String) {
+        launch {
             _model.value = UiModel.Loading
-            val result = withContext(Dispatchers.IO){
-                assignAdopterToPetUseCase.invoke(petId,adopterId)
+            val result = withContext(Dispatchers.IO) {
+                assignAdopterToPetUseCase.invoke(petId, adopterId)
             }
-            if(result){
+            if (result) {
                 showSuccessAdvice()
-            }else {
+            } else {
                 showRegistrationError()
             }
         }
---
+++ app/src/test/java/com/teammovil/pettracker/assignadoptertopet/AssignAdopterViewModelTest.kt
@@ -0,0 +1,67 @@
+package com.teammovil.pettracker.assignadoptertopet
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import com.teammovil.pettracker.ui.assigningadoptertopet.AdopterViewModel
+import com.teammovil.pettracker.util.MessageValidation
+import com.teammovil.testshared.mockAdopter
+import com.teammovil.testshared.mockPet
+import com.teammovil.usecases.assignadoptertopet.AssignAdopterToPetUseCase
+import com.teammovil.usecases.getalladopters.GetAllAdoptersUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+class AssignAdopterViewModelTest {
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var assignAdopterToPetUseCase: AssignAdopterToPetUseCase
+
+    @Mock
+    lateinit var getAllAdoptersUseCase: GetAllAdoptersUseCase
+
+    @Mock
+    lateinit var observer: Observer<AdopterViewModel.UiModel>
+
+    private lateinit var vm: AdopterViewModel
+
+    @Before
+    fun setUp(){
+        MockitoAnnotations.initMocks(this)
+        vm = AdopterViewModel(assignAdopterToPetUseCase,getAllAdoptersUseCase,Dispatchers.Unconfined)
+    }
+
+    @Test
+    fun `when called get all adopters use case, getAllAdopters is called`(){
+        runBlocking {
+            val adopters = listOf(mockAdopter.copy())
+
+            whenever(getAllAdoptersUseCase.invoke()).thenReturn(adopters)
+            vm.model.observeForever(observer)
+
+            vm.onStartView()
+            verify(getAllAdoptersUseCase).invoke()
+            verify(observer).onChanged(AdopterViewModel.UiModel.AdoptersContent(adopters))
+        }
+    }
+
+    @Test
+    fun `when called assign adopter use case, show success notification is called`(){
+        runBlocking {
+            whenever(assignAdopterToPetUseCase.invoke(petId= mockPet.id,adopterId = mockAdopter.email)).thenReturn(true)
+            vm.model.observeForever(observer)
+
+            vm.onAssignAdopterToPet(adopterId = mockAdopter.email,petId = mockPet.id)
+            verify(observer).onChanged(AdopterViewModel.UiModel.SuccessNotification(MessageValidation.ADOPTER_REGISTER_SUCCESS))
+        }
+    }
+
+}
